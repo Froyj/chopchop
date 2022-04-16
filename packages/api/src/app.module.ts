750f3644c4e609cbd2a06fcfb2f72e328ec42535
Geoffroy Periot <geoffroy.periot.dev@gmail.com>
2022-04-16 20:04:00 +0200

    scaffolding the 'products' domain

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:example@localhost:27017', {
+      dbName: 'copabanana',
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
