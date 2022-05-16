83199dfa58a2caa3b2873ad9c8e78373cca6e641
Geoffroy Periot <geoffroy.periot.dev@gmail.com>
2022-05-16 15:09:08 +0200

    missing controller methods

--- packages/webapp/src/api/ProductController.ts
+++ packages/webapp/src/api/ProductController.ts
@@ -1,26 +1,20 @@
+import { Product, ProductDto } from 'types/Product';
 import axios from '../helpers/axios-config';
 
-interface ReheatInstructions {
-  mode: string;
-  instructions: string;
-}
-interface ProductDto {
-  _id: string;
-  name: string;
-  description: string;
-  nutritionalInformation: string[];
-  retentionTime: number;
-  reheatingInstructions: ReheatInstructions;
-  availability: string;
-  servings: number;
-  imageUrl: string;
-};
 class ProductController {
   public static getAll() {
     return axios.get('/products').then((res) => res.data);
   }
-  public static create(productDto: ProductDto) {
-    return axios.post('/products', productDto).then(res => res.data);
+  public static create(product: ProductDto) {
+    return axios.post('/products', product).then(res => res.data);
+  }
+
+  public static update(product: Product) {
+    return axios.patch(`/products/${product._id}`, product).then(res => res.data);
+  }
+
+  public static delete(product: Product) {
+    return axios.delete(`/products/${product._id}`).then(res => res.data);
   }
 }
 
